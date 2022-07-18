308556a1d56508ead875aadea5b2212a3980881b
GabrielLucasSBG <gabrielbenfica@hotmail.com>
2022-07-18 13:21:06 -0300

    Criando rotas iniciais

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\{
-    AuthController,
+use App\Http\Controllers\{AuthController,
     BilletController,
     DocController,
     FoundAndLostController,
@@ -14,6 +13,49 @@ use App\Http\Controllers\{
     WarningController,
 };
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/401', [AuthController::class, 'unauthorized'])->name('login');
+
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/register', [AuthController::class, 'register']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::post('/auth/validate', [AuthController::class, 'validateToken']);
+    Route::post('/auth/logou', [AuthController::class, 'logout']);
+
+    //MURAL DE AVISOS
+    Route::get('/walls', [WallController::class, 'getAll']);
+    Route::post('/wall/{id}/like', [WallController::class, 'like']);
+
+    //DOCUMENTOS
+    Route::get('/docs', [DocController::class, 'getAll']);
+
+    //LIVRO DE OCORRÊCIAS
+    Route::get('/warnings', [WarningController::class, 'getMyWarnings']);
+    Route::post('/warning', [WarningController::class, 'setWarning']);
+    Route::post('/warning/file', [WarningController::class, 'addWarningFile']);
+
+    //BOLETOS
+    Route::get('/billets', [BilletController::class, 'getAll']);
+
+    //ACHADOS E PERDIDOS
+    Route::get('/foundandlost', [FoundAndLostController::class, 'getAll']);
+    Route::post('/foundandlost', [FoundAndLostController::class, 'insert']);
+    Route::put('/foundandlost{id}', [FoundAndLostController::class, 'update']);
+
+    //UNIDADE
+    Route::get('/unit/{id}', [UnitController::class, 'getInfo']);
+    Route::post('/unit/{id}/addperson', [UnitController::class, 'addperson']);
+    Route::post('/unit/{id}/addvehicle', [UnitController::class, 'addvehicle']);
+    Route::post('/unit/{id}/addpet', [UnitController::class, 'addpet']);
+    Route::post('/unit/{id}/removeperson', [UnitController::class, 'removeperson']);
+    Route::post('/unit/{id}/removevehicle', [UnitController::class, 'removevehicle']);
+    Route::post('/unit/{id}/removepet', [UnitController::class, 'removepet']);
+
+    //RESERVAS
+    Route::get('/reservations', [ReservationController::class, 'getReservations']);
+    Route::get('/myreservations', [ReservationController::class, 'getMyReservations']);
+    Route::delete('myreservation/{id}', [ReservationController::class, 'delMyReservation']);
+    Route::post('reservation/{id}', [ReservationController::class, 'setReservation']);
+    Route::get('/reservation/{id}/disableddates', [ReservationController::class, 'getDisabledDates']);
+    Route::get('/reservation/{id}/times', [ReservationController::class, 'getTimes']);
 });
