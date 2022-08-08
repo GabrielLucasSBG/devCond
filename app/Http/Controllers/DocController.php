368d198f581947626fc0fd45a61765024d185e9b
GabrielLucasSBG <gabrielbenfica@hotmail.com>
2022-08-08 12:25:54 -0300

    Criando get all docs

--- app/Http/Controllers/DocController.php
+++ app/Http/Controllers/DocController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Doc;
 use Illuminate\Http\Request;
 
 class DocController extends Controller
 {
-    //
+    public function getAll()
+    {
+        $array = ['error' => ''];
+
+        $docs = Doc::all();
+
+        foreach ($docs as $key => $doc) {
+            $docs[$key]['fileurl'] = asset('storage/'.$doc['fileurl']);
+        }
+
+        $array['list'] = $docs;
+        return $array;
+    }
 }
